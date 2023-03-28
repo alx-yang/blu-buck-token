164c4afc7b9696e074dfa9bc099c4de2898d661b
Mihir Kelkar <mihkel@umich.edu>
2023-03-28 10:18:36 -0400

    bc i have to

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.0/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
