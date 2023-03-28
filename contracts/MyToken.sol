cce92bfaeb3eb196e28fd619c02377fbe82288ae
Mihir Kelkar <mihkel@umich.edu>
2023-03-28 19:31:59 -0400

    buy food function

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,13 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.0/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    constructor() ERC20("MyToken", "MTK") {}
+
+    address private umich;
+    constructor() ERC20("BlueBuckToken", "BBT") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+
+    function buyFood(address from, uint256 amount) public {
+        require (balanceOf(from) - amount >= 0);
+        _transfer(from, umich, amount);
+    }
+
 }
