f2b396e6b4ae67f92c17773e612bf159f8b7d9df
Alex Yang <alxyang@umich.edu>
2023-03-28 19:43:49 -0400

    created struct

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -6,7 +6,22 @@ import "node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
 
-    address private umich;
+    address private owner; // University of Michigan
+
+    mapping(address => Student.uniqname);
+    mapping(Student.uniqname => address);
+
+    struct Student {
+        string uniqname;
+        string name;
+        string grade;
+        string diningPlan;
+        string school;
+        string major;
+        uint32 UMID;
+    }
+
+
     constructor() ERC20("BlueBuckToken", "BBT") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
